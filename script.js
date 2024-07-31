4076985bbc5d315c3c2ee3556616a07f2824371e
Elizabeth Clawson <emclawson1@gmail.com>
2024-07-31 14:57:18 -0500

    completed FizzBuzz

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello")
+
+
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 === 0 ) {
+        console.log("fizz")
+    }
+    else if(i % 5 === 0 ) {
+        console.log("buzz")
+    }
+    else (i % 3 !== 0 && i % 5 !== 0);{
+        console.log(i)
+    }
+}
